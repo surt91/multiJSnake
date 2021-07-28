3fa6b8dd8743980ab69e8e61fb34fbb5f8c29607
Hendrik Schawe <surt91@gmail.com>
2021-07-28 14:02:28 +0200

    initial commit

---
+++ src/main/java/me/schawe/RESTfulSnake/ResTfulSnakeApplication.java
@@ -0,0 +1,13 @@
+package me.schawe.RESTfulSnake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulSnakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulSnakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/schawe/RESTfulSnake/ResTfulSnakeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.schawe.RESTfulSnake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulSnakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
