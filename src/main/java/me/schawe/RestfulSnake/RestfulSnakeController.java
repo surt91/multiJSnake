49a48f8878805ac162ae2afe51b455f7204cd53b
Hendrik Schawe <surt91@gmail.com>
2021-07-29 11:15:46 +0200

    remove unneeded imports

--- src/main/java/me/schawe/RestfulSnake/RestfulSnakeController.java
+++ src/main/java/me/schawe/RestfulSnake/RestfulSnakeController.java
@@ -1,10 +1,6 @@
 package me.schawe.RestfulSnake;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.HashMap;
 
