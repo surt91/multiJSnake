04f821810f899e98670373cbccee57d5cc3114d2
Hendrik Schawe <surt91@gmail.com>
2021-07-29 10:12:28 +0200

    fixed lint

--- src/main/java/me/schawe/RestfulSnake/Snake.java
+++ src/main/java/me/schawe/RestfulSnake/Snake.java
@@ -72,13 +72,11 @@ public class Snake {
         int targetStringLength = 10;
         Random random = new Random();
 
-        String generatedString = random.ints(leftLimit, rightLimit + 1)
+        return random.ints(leftLimit, rightLimit + 1)
                 .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
                 .limit(targetStringLength)
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                 .toString();
-
-        return generatedString;
     }
 
     public void add_food() {
