5f86241052d6804e611a17ea284fedd7d8a3f3bf
Hendrik Schawe <hendrik.schawe@codecentric.de>
2021-10-12 11:49:44 +0200

    move the py4j initialization into a CLR component

--- src/main/java/me/schawe/multijsnake/snake/PythonEntry.java
+++ src/main/java/me/schawe/multijsnake/snake/PythonEntry.java
@@ -1,6 +1,15 @@
 package me.schawe.multijsnake.snake;
 
-public class PythonEntry {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import py4j.GatewayServer;
+
+// This class enables external calls from python to the GameState object.
+// This is especially useful to apply the vast machine learning ecosystem
+// of Python to train an AI to steer the snake.
+
+@Component
+public class PythonEntry implements CommandLineRunner {
 
     private final GameState gameState;
 
@@ -10,5 +19,11 @@ public class PythonEntry {
 
     public GameState getGameState() {
         return gameState;
+    }
+
+    public void run(String... args) {
+        GatewayServer gatewayServer = new GatewayServer(new PythonEntry());
+        gatewayServer.start();
+        System.out.println("Python Gateway Server Started");
     }
 }
