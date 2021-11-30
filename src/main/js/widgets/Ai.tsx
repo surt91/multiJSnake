e89f407982da3789531e400c3e209fbaf8cc92ff
Hendrik Schawe <hendrik.schawe@codecentric.de>
2021-11-30 15:56:31 +0100

    directly call draw
    this should have performance benefits

--- src/main/js/widgets/Ai.tsx
+++ src/main/js/widgets/Ai.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import Canvas from "../visualization/canvas";
+import React, {useEffect, useRef, useState} from "react";
 import {defaultVisualizationOptions, draw} from "../visualization/canvasDraw";
 import {Container, Grid, Stack} from "@mui/material";
 // @ts-ignore
@@ -22,118 +21,111 @@ export type AiOption = {
     input: string,
     mode: string
 }
-type Props = {};
-type State = {
-    game: JsGameState,
-    currentModel: AiOption
-};
 
-export default class Ai extends React.Component<Props, State> {
-    private readonly aiJsOptions: AiOption[];
-    private model_promise: Promise<LayersModel> | null;
-    private refresh?: number;
+export default function Ai() {
+    const aiJsOptionsAll = jsYaml.load(rawAiOptions);
+    const aiJsOptions: AiOption[] = aiJsOptionsAll.filter((obj: AiOption) => "model_path_js" in obj);
 
-    constructor(props: Props) {
-        super(props);
+    const visOpts = defaultVisualizationOptions;
 
-        const aiJsOptionsAll = jsYaml.load(rawAiOptions);
-        const aiJsOptions = aiJsOptionsAll.filter((obj: AiOption) => "model_path_js" in obj);
+    const [game, setGame] = useState(new JsGameState(10, 10, (score) => gameOver(score)))
+    const [modelOption, setModelOption] = useState(aiJsOptions[aiJsOptions.length - 1]);
+    const canvasRef = useRef<HTMLCanvasElement|null>(null);
 
-        this.aiJsOptions = aiJsOptions;
+    let model_promise: Promise<LayersModel> | null = null;
 
-        this.state = {
-            game: new JsGameState(10, 10, (score) => this.gameOver(score)),
-            currentModel: aiJsOptions[aiJsOptions.length - 1]
+    useEffect(() => {
+        setAi(modelOption);
+        const refresh = window.setInterval(() => step(), 30);
+
+        const canvas = canvasRef.current;
+        if(canvas === null) {
+            throw "Canvas failed to construct";
+        }
+        const contextTest = canvas.getContext('2d');
+        if(contextTest === null) {
+            throw "Failed to get the Context of the constructed canvas";
+        }
+        const context: CanvasRenderingContext2D = contextTest;
+
+        function step() {
+            model_promise && model_promise.then(model => {
+                engine().startScope()
+
+                if(modelOption.input === "global") {
+                    const state = tensor([game.trainingBitmap()]);
+                    const out = model.predict(state);
+                    // @ts-ignore
+                    const action = out[0].argMax(1).arraySync()[0];
+                    game.absoluteAction2Move(action);
+                } else {
+                    const state = tensor([game.trainingState()]);
+                    const out = model.predict(state);
+                    // @ts-ignore
+                    const action = out[0].argMax(1).arraySync()[0];
+                    game.relativeAction2Move(action);
+                }
+                engine().endScope();
+
+                game.update();
+
+                setGame(game);
+
+                draw(context, game, visOpts)
+            });
+        }
+
+        return () => {
+            window.clearInterval(refresh);
+            freeModel();
         };
+    })
 
-        this.model_promise = null;
+    // function newGame(width: number, height: number) {
+    //     const game = new JsGameState(width, height, (score) => gameOver(score));
+    //     setGame(game);
+    // }
+
+    function freeModel(){
+        model_promise && model_promise.then(model => model.layers.forEach(l => l.dispose()));
     }
 
-    componentDidMount() {
-        this.setAi(this.state.currentModel)
-        this.refresh = window.setInterval(() => this.step(), 30);
-    }
-
-    componentWillUnmount() {
-        this.refresh && window.clearInterval(this.refresh);
-        this.freeModel();
-    }
-
-    newGame(width: number, height: number) {
-        const game = new JsGameState(width, height, (score) => this.gameOver(score));
-        this.setState({game: game});
-    }
-
-    freeModel(){
-        this.model_promise && this.model_promise.then(model => model.layers.forEach(l => l.dispose()));
-    };
-
-    setAi(obj: AiOption) {
-        this.setState({currentModel: obj});
+    function setAi(obj: AiOption) {
+        setModelOption(obj);
         // if there is an old model, dispose it first
-        this.freeModel();
-        this.model_promise = loadLayersModel(obj.model_path_js);
+        freeModel();
+        model_promise = loadLayersModel(obj.model_path_js);
     }
 
-    gameOver(score: number) {
-        console.log(score);
+    function gameOver(score: number) {
+        console.log(score)
     }
 
-    step() {
-        this.model_promise && this.model_promise.then(model => {
-            engine().startScope()
-
-            if(this.state.currentModel.input === "global") {
-                const state = tensor([this.state.game.trainingBitmap()]);
-                const out = model.predict(state);
-                // @ts-ignore
-                const action = out[0].argMax(1).arraySync()[0];
-                this.state.game.absoluteAction2Move(action);
-            } else {
-                const state = tensor([this.state.game.trainingState()]);
-                const out = model.predict(state);
-                // @ts-ignore
-                const action = out[0].argMax(1).arraySync()[0];
-                this.state.game.relativeAction2Move(action);
-            }
-            engine().endScope();
-
-            this.state.game.update();
-
-            this.setState({game: this.state.game});
-        });
-    }
-
-    render() {
-        const options = defaultVisualizationOptions;
-
-        return (
-            <Container maxWidth="lg">
-                <Grid container spacing={4} pt={4} justifyContent="space-around" alignItems="flex-start">
-                    <Grid item xs={12} lg={6}>
-                        <Canvas
-                            draw={ctx => draw(ctx, this.state.game, options)}
-                            width={this.state.game.width * options.scale}
-                            height={this.state.game.height * options.scale}
-                            sx={{ mx: "auto" }}
-                            focused={_ => {}}
-                        />
-                    </Grid>
-                    <Grid item xs={12} lg={6}>
-                        <Stack spacing={2}>
-                            <AddAutopilot
-                                onCommit={id => this.setAi(id)}
-                                onChange={id => this.setAi(id)}
-                                aiOptions={this.aiJsOptions}
-                                submitText={"Change AI"}
-                                width={500}
-                                defaultValue={this.state.currentModel}
-                                commitMode={false}
-                            />
-                        </Stack>
-                    </Grid>
+    return (
+        <Container maxWidth="lg">
+            <Grid container spacing={4} pt={4} justifyContent="space-around" alignItems="flex-start">
+                <Grid item xs={12} lg={6}>
+                    <canvas
+                        ref={canvasRef}
+                        width={game.width * visOpts.scale}
+                        height={game.height * visOpts.scale}
+                        id={"snakeCanvas"}
+                    />
                 </Grid>
-            </Container>
-        )
-    }
+                <Grid item xs={12} lg={6}>
+                    <Stack spacing={2}>
+                        <AddAutopilot
+                            onCommit={id => setAi(id)}
+                            onChange={id => setAi(id)}
+                            aiOptions={aiJsOptions}
+                            submitText={"Change AI"}
+                            width={500}
+                            defaultValue={modelOption}
+                            commitMode={false}
+                        />
+                    </Stack>
+                </Grid>
+            </Grid>
+        </Container>
+    )
 }
