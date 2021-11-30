e9dc27563004e47162eaf9d8e7912cb98068c4c8
Hendrik Schawe <hendrik.schawe@codecentric.de>
2021-11-30 11:51:13 +0100

    serve precompressed bundles
    using both gzip and brotli depending on the "Accept-Encoding" header

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const CompressionPlugin = require("compression-webpack-plugin");
+const zlib = require("zlib");
+
 module.exports = {
     entry: './src/main/js/App.tsx',
     cache: true,
@@ -23,5 +26,26 @@ module.exports = {
     },
     experiments: {
         topLevelAwait: true
-    }
+    },
+    plugins: [
+        new CompressionPlugin({
+            filename: "[path][base].gz",
+            algorithm: "gzip",
+            test: /\.js$|\.css$|\.html$/,
+            threshold: 10240,
+            minRatio: 0.8,
+        }),
+        new CompressionPlugin({
+            filename: "[path][base].br",
+            algorithm: "brotliCompress",
+            test: /\.(js|css|html|svg)$/,
+            compressionOptions: {
+                params: {
+                    [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+                },
+            },
+            threshold: 10240,
+            minRatio: 0.8,
+        }),
+    ],
 };
