3c85fd0eafca0349764dc56a1da31d8452f41500
Hendrik Schawe <surt91@gmail.com>
2022-01-10 13:09:51 +0100

    run tests for the static version

--- buildStatic.sh
+++ buildStatic.sh
@@ -5,11 +5,17 @@
 # copy sources
 rm -rf tmp/
 mkdir tmp/
+
 cp -r src/main/js/ tmp/
 cp webpack.config.js package.json tsconfig.json package-lock.json cypress.json tmp/
+
 mkdir tmp/resources/
 cp -r src/main/resources/{models,static} tmp/resources/
 cp src/main/resources/templates/main.html tmp/resources/static/index.html
+
+# copy e2e tests, but delete those which depend on the backend
+cp -r cypress tmp/
+rm tmp/cypress/integration/{auth,canvas,steering}.js
 
 # patch the main component
 cd tmp || exit 1
@@ -20,6 +26,8 @@ sed -i 's|./src/main/js/App.tsx|./js/App.tsx|g' webpack.config.js
 sed -i 's|path: __dirname + "/src/main/resources/static/built"|path: __dirname + "/resources/static/built"|g' webpack.config.js
 sed -i 's|xmlns:th="http://www.thymeleaf.org"||g' resources/static/index.html
 sed -i '/th:content/d' resources/static/index.html
+
+sed -i 's|cy.visit("/ai");|cy.visit("/");|'g cypress/integration/ai.js
 
 python3 << EOF
 def snip(filename):
@@ -41,10 +49,8 @@ for file in glob("js/**/*.[tj]s*", recursive=True):
 EOF
 
 # build
-npm install
-npm run build
-
-# TODO: test the static version
+npm install || exit 1
+npm run build || exit 1
 
 echo "build finished"
 echo "now deploy the folder 'tmp/resources/static'"
--- cypress/integration/canvas.js
+++ cypress/integration/canvas.js
@@ -1,39 +1,3 @@
-async function ensureChange(name1, name2) {
-    // https://gambini.me/en/blog/comparing-website-screenshots-with-cypress-and-pixelmatch
-    // PNGJS lets me load the picture from disk
-    const PNG = require('pngjs').PNG;
-    // pixelmatch library will handle comparison
-    const pixelmatch = require('pixelmatch');
-
-    cy.readFile(
-        './cypress/snapshots/actual/canvas.js/' + name1 + '.png', 'base64'
-    ).then(first =>
-        cy.readFile(
-            './cypress/snapshots/actual/canvas.js/' + name2 + '.png', 'base64'
-        ).then(second => {
-            // load both pictures
-            const img1 = PNG.sync.read(Buffer.from(first, 'base64'));
-            const img2 = PNG.sync.read(Buffer.from(second, 'base64'));
-
-            const {width, height} = img1;
-            const diff = new PNG({width, height});
-
-            // calling pixelmatch return how many pixels are different
-            const numDiffPixels = pixelmatch(img1.data, img2.data, diff.data, width, height);
-
-            // calculating a percent diff
-            const diffPercent = (numDiffPixels / (width * height) * 100);
-
-            cy.log(`Found a ${diffPercent.toFixed(2)}% pixel difference`);
-
-            // if more than 2 pixels change (and this should be a 10x10 grid), we can be sure to not have
-            // accidentially made screenshots during initialization, where head and food are places, but that
-            // something is actually moving
-            expect(diffPercent).to.be.above(2);
-        })
-    );
-}
-
 describe('Canvas Test', () => {
     it("visual test of the canvas", () => {
         cy.visit('/');
@@ -96,29 +60,5 @@ describe('Canvas Test', () => {
         cy.get("button").contains("Unpause").click();
         cy.wait(200);
         cy.get('canvas').compareSnapshot('canvas-ai-step', 0.0);
-    })
-
-    it("is ai moving", {
-        defaultCommandTimeout: 20000
-    }, () => {
-        cy.visit('/ai');
-
-        cy.get('canvas').screenshot('zeroth');
-        cy.wait(2000);
-        cy.get('canvas').screenshot('first');
-        cy.wait(1000);
-        cy.get('canvas').screenshot('second');
-
-        ensureChange("first", "second");
-
-        cy.get("#aiChooser").click();
-        cy.contains("Actor-Critic (n=100)").click();
-        cy.get('canvas').screenshot('zeroth2');
-        cy.wait(2000);
-        cy.get('canvas').screenshot('third');
-        cy.wait(1000);
-        cy.get('canvas').screenshot('fourth');
-
-        ensureChange("third", "fourth");
     })
 })
